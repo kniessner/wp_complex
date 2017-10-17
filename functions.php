5dac033b3dbc95d1824d67e6bc096de67c24696a
Sascha-Darius Knießner <Dariuskniessner@littlewebgiants.com>
2017-10-17 05:33:42 +0200

    new core

---
+++ functions.php
@@ -0,0 +1,67 @@
+<?php
+/* Sets the path to the core framework directory. */
+if ( !defined( 'HYBRID_DIR' ) )
+	define( 'HYBRID_DIR', trailingslashit( TEMPLATEPATH ) . '/core/hybrid/' );
+
+/* Sets the path to the core framework directory URI. */
+if ( !defined( 'HYBRID_URI' ) )
+	define( 'HYBRID_URI', trailingslashit( TEMPLATEPATH ) . '/core/hybrid/hybrid.php' );
+/* Load the core theme framework. */
+require_once( trailingslashit( TEMPLATEPATH ) . '/core/hybrid/hybrid.php' );
+$theme = new Hybrid();
+
+/**
+ * Theme setup function.  This function adds support for theme features and defines the default theme
+ * actions and filters.
+ *
+ * @since 0.1.0
+ */
+
+
+add_action( 'after_setup_theme', 'hybrid_theme_setup' );
+function hybrid_theme_setup() {
+
+	/* Add theme support for core framework features. */
+	add_theme_support( 'hybrid-core-widgets' );
+	add_theme_support( 'hybrid-core-template-hierarchy' );
+	add_theme_support( 'loop-pagination' );
+	add_theme_support( 'get-the-image' );
+	add_theme_support( 'post-thumbnails' );
+	add_theme_support( 'automatic-feed-links' );
+	add_theme_support( 'cleaner-caption' );
+	add_theme_support( 'cleaner-gallery' );
+
+	// add_theme_support( 'breadcrumb-trail' );
+	// add_theme_support( 'hybrid-core-post-meta-box' );
+	 add_theme_support( 'hybrid-core-theme-settings' );
+	// add_theme_support( 'hybrid-core-seo' );
+}
+
+
+add_action( 'after_setup_theme', 'additional_setup' );
+function additional_setup() {
+
+		require_once( trailingslashit( TEMPLATEPATH ) . 'settings.php' );
+
+}
+
+
+/**
+ * Enqueue additional scripts here.
+ *
+ *
+ */
+
+add_action('wp_enqueue_scripts', 'add_scripts');
+
+function add_scripts() {
+		/*wp_enqueue_script('bootstrap',
+		 	get_template_directory_uri() . '/src/js/vendor/bootstrap.min.js',
+		 	array("jquery"),
+		 	'3.0.0', true );*/
+	   }
+
+
+
+?>
+
---
+++ settings.php
@@ -0,0 +1,49 @@
+<?php
+
+/* Register custom menus. */
+add_action( 'init', 'register_menus', 5 );
+
+
+function register_menus() {
+	register_nav_menu( 'primary',    _x( 'Primary',    'nav menu location', 'hybrid-base' ) );
+	register_nav_menu( 'secondary',  _x( 'Secondary',  'nav menu location', 'hybrid-base' ) );
+	register_nav_menu( 'subsidiary', _x( 'Subsidiary', 'nav menu location', 'hybrid-base' ) );
+}
+
+
+
+/* Register sidebars. */
+add_action( 'widgets_init', 'register_sidebars', 5 );
+function register_sidebars() {
+
+	hybrid_register_sidebar(
+		array(
+			'id'          => 'posts',
+			'name'        => _x( 'Posts', 'sidebar', 'k_complex' ),
+			'description' => __( 'Shown on posts and blog index.', 'k_complex' )
+		)
+	);
+
+	hybrid_register_sidebar(
+		array(
+			'id'          => 'pages',
+			'name'        => _x( 'Pages', 'sidebar', 'k_complex' ),
+			'description' => __( 'Shown on pages.', 'k_complex' )
+		)
+	);
+}
+
+
+add_action( 'admin_menu', 'remove_menus' );
+function remove_menus(){
+  
+  remove_menu_page( 'index.php' );                  //Dashboard
+  remove_menu_page( 'jetpack' );                    //Jetpack* 
+  remove_menu_page( 'edit.php' );                   //Posts
+  remove_menu_page( 'edit-comments.php' );          //Comments
+  //remove_menu_page( 'themes.php' );                 //Appearance
+
+  
+}
+
+?>
