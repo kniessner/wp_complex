cb94fb1a9f567419a1de70092297bc453ef585bd
Sascha-Darius Knießner <Dariuskniessner@littlewebgiants.com>
2017-10-17 22:10:39 +0200

    automatic commit

---
+++ footer.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Footer Template
+ *
+ * The footer template is generally used on every page of your site. Nearly all other
+ * templates call it somewhere near the bottom of the file. It is used mostly as a closing
+ * wrapper, which is opened with the header.php file. It also executes key functions needed
+ * by the theme, child themes, and plugins. 
+ *
+ */
+?>  </div> <!--#main-->
+        </div> <!--#innerwrap-->
+            </div> <!--#outerwrap-->
+            <footer id="footer">
+              
+            </footer>      
+            
+        <?php wp_footer(); ?> 
+    </body>
+</html>
