efd83d62b8580855540085bdc6b42adc763ed258
Sascha-Darius Knießner <Dariuskniessner@littlewebgiants.com>
2017-10-21 23:22:07 +0200

    automatic commit

--- functions.php
+++ functions.php
@@ -38,8 +38,7 @@ add_action( 'after_setup_theme', 'complex_additional_setup' );
 
 function complex_additional_setup() {
 		require_once( trailingslashit( TEMPLATEPATH ) . 'wp_setup/admin/settings.php' );
-		require_once( trailingslashit( TEMPLATEPATH ) . 'wp_setup/admin/custom_post-type.php' );
-		//require_once( trailingslashit( TEMPLATEPATH ) . 'src/modules/mediathek.php' );
+
 
 }
 
