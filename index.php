cae7a51f17cd7a45e88861915c65af9d17ab2f4f
Sascha-Darius Knießner <Dariuskniessner@littlewebgiants.com>
2017-10-22 22:03:26 +0200

    automatic commit

--- index.php
+++ index.php
@@ -18,7 +18,10 @@ get_header(); ?>
 		<main id="main" class="site-main">
 		<!--<div class="start_image" ><img class="screen_fit" src="<?php bloginfo('template_url');?>/src/img/logo_form.png" /> </div>-->
 		<div id="Orbit"></div>
-
+			<object type="image/svg+xml" data="<?php bloginfo('template_url');?>/src/img/logo_Fokus.svg">
+			  <!-- fallback -->
+			  <img src="<?php bloginfo('template_url');?>/src/img/logo_Fokus.png" alt="">
+			</object>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
