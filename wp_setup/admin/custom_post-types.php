d2350fcb71d1663ed9fafabb62bf6d78fdcbc2c2
Sascha-Darius Knießner <Dariuskniessner@littlewebgiants.com>
2017-10-22 20:22:09 +0200

    automatic commit

--- wp_setup/admin/custom_post-types.php
+++ wp_setup/admin/custom_post-types.php
@@ -63,7 +63,55 @@ function create_custom_post_types() {
 			);
 	register_post_type( 'projects', $args );
 
-
+		$labels = array(
+				'name'                  => _x( 'Clients', 'Post Type General Name', 'complex' ),
+				'singular_name'         => _x( 'Client', 'Post Type Singular Name', 'complex' ),
+				'menu_name'             => __( 'Clients', 'complex' ),
+				'name_admin_bar'        => __( 'Post Type', 'complex' ),
+				'archives'              => __( 'Item Archives', 'complex' ),
+				'attributes'            => __( 'Item Attributes', 'complex' ),
+				'parent_item_colon'     => __( 'Parent Item:', 'complex' ),
+				'all_items'             => __( 'All Items', 'complex' ),
+				'add_new_item'          => __( 'Add New Item', 'complex' ),
+				'add_new'               => __( 'Add New', 'complex' ),
+				'new_item'              => __( 'New Clients', 'complex' ),
+				'edit_item'             => __( 'Edit Item', 'complex' ),
+				'update_item'           => __( 'Update Item', 'complex' ),
+				'view_item'             => __( 'View Item', 'complex' ),
+				'view_items'            => __( 'View Items', 'complex' ),
+				'search_items'          => __( 'Search Item', 'complex' ),
+				'not_found'             => __( 'Not found', 'complex' ),
+				'not_found_in_trash'    => __( 'Not found in Trash', 'complex' ),
+				'featured_image'        => __( 'Featured Image', 'complex' ),
+				'set_featured_image'    => __( 'Set featured image', 'complex' ),
+				'remove_featured_image' => __( 'Remove featured image', 'complex' ),
+				'use_featured_image'    => __( 'Use as featured image', 'complex' ),
+				'insert_into_item'      => __( 'Insert into item', 'complex' ),
+				'uploaded_to_this_item' => __( 'Uploaded to this item', 'complex' ),
+				'items_list'            => __( 'Items list', 'complex' ),
+				'items_list_navigation' => __( 'Items list navigation', 'complex' ),
+				'filter_items_list'     => __( 'Filter items list', 'complex' ),
+			);
+			$args = array(
+				'label'                 => __( 'Client', 'complex' ),
+				'description'           => __( 'Post Type Description', 'complex' ),
+				'labels'                => $labels,
+				'supports'              => array( ),
+				'taxonomies'            => array( 'category', 'post_tag' ),
+				'hierarchical'          => false,
+				'public'                => true,
+				'show_ui'               => true,
+				'show_in_menu'          => true,
+				'menu_position'         => 5,
+				'show_in_admin_bar'     => true,
+				'show_in_nav_menus'     => true,
+				'can_export'            => true,
+				'has_archive'           => true,		
+				'exclude_from_search'   => false,
+				'publicly_queryable'    => true,
+				'capability_type'       => 'page',
+			);
+			register_post_type( 'client', $args );
 
 
 		
