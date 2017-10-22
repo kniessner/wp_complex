1541a4f5e415f8d028e0a23f9c1535f1ae321f88
Sascha-Darius Knießner <Dariuskniessner@littlewebgiants.com>
2017-10-22 15:23:52 +0200

    automatic commit

--- core/js/bundle.js
+++ core/js/bundle.js
@@ -61,7 +61,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "cf49970922aff4de92d9"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "43d90243a88037cccaba"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
@@ -62372,19 +62372,27 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 
 var OrbitControls = __webpack_require__(18)(__WEBPACK_IMPORTED_MODULE_0_three__)
 		
-var scene = new __WEBPACK_IMPORTED_MODULE_0_three__["Scene"]();
-var camera = new __WEBPACK_IMPORTED_MODULE_0_three__["PerspectiveCamera"]( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+			var scene = new __WEBPACK_IMPORTED_MODULE_0_three__["Scene"]();
+			var camera = new __WEBPACK_IMPORTED_MODULE_0_three__["PerspectiveCamera"]( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+			var renderer = new __WEBPACK_IMPORTED_MODULE_0_three__["WebGLRenderer"]();
 
-var renderer = new __WEBPACK_IMPORTED_MODULE_0_three__["WebGLRenderer"]();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.getElementById('Orbit').appendChild( renderer.domElement );
-		
-var geometry = new __WEBPACK_IMPORTED_MODULE_0_three__["BoxGeometry"]( 1, 1, 1 );
-var material = new __WEBPACK_IMPORTED_MODULE_0_three__["MeshBasicMaterial"]( { color: 0x00ff00 } );
-var cube = new __WEBPACK_IMPORTED_MODULE_0_three__["Mesh"]( geometry, material );
-scene.add( cube );
+			renderer.setSize( window.innerWidth, window.innerHeight );
+			document.getElementById('Orbit').appendChild( renderer.domElement );
 
-camera.position.z = 5;
+			
+			var geometry = new __WEBPACK_IMPORTED_MODULE_0_three__["BoxGeometry"]( 1, 1, 1 );
+			var material = new __WEBPACK_IMPORTED_MODULE_0_three__["MeshBasicMaterial"]( { color: 0x00ff00 } );
+			var cube = new __WEBPACK_IMPORTED_MODULE_0_three__["Mesh"]( geometry, material );
+			scene.add( cube );
+
+
+			camera.position.z = 5;
+			var animate = function () {
+				requestAnimationFrame( animate );
+				cube.rotation.x += 0.1;
+				cube.rotation.y += 0.1;
+				renderer.render(scene, camera);
+			};
 
 /***/ })
 /******/ ]);
