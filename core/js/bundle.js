962950d399e45678f9b41673b2f8586c8c2836cd
Sascha-Darius Knießner <Dariuskniessner@littlewebgiants.com>
2017-10-22 16:07:52 +0200

    automatic commit

--- core/js/bundle.js
+++ core/js/bundle.js
@@ -61,7 +61,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "72b1d99ee490b2ebaa7e"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "6825257d692a3005e27e"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
@@ -17114,233 +17114,109 @@ var OrbitControls = __webpack_require__(18)(__WEBPACK_IMPORTED_MODULE_0_three__)
 jQuery(document).ready(function($) {
 
 
-var scene = new __WEBPACK_IMPORTED_MODULE_0_three__["Scene"]();
-var camera = new __WEBPACK_IMPORTED_MODULE_0_three__["PerspectiveCamera"]( 60, (window.innerWidth-15) / (window.innerHeight-15), 1, 1000 );
-var renderer = new __WEBPACK_IMPORTED_MODULE_0_three__["WebGLRenderer"]();
-var raycaster;
+	var scene = new __WEBPACK_IMPORTED_MODULE_0_three__["Scene"]();
+	var camera = new __WEBPACK_IMPORTED_MODULE_0_three__["PerspectiveCamera"]( 60, (window.innerWidth-15) / (window.innerHeight-15), 1, 1000 );
+	var renderer = new __WEBPACK_IMPORTED_MODULE_0_three__["WebGLRenderer"]();
+	var raycaster;
+			
+	renderer.setSize( window.innerWidth-15, (window.innerHeight) );
+	document.getElementById('Orbit').appendChild( renderer.domElement );
+	camera.position.z = 520;
+	camera.position.x = -0;
+
+
+	/*=================================================
+		MAIN ELEMENTS
+	=================================================*/	
+
+
+	var geometry = new __WEBPACK_IMPORTED_MODULE_0_three__["TorusGeometry"]( 202, 15, 10, 150);
+	var material = new __WEBPACK_IMPORTED_MODULE_0_three__["MeshLambertMaterial"]( { color:  "rgba(4, 4, 4,1)" , morphTargets:true, wireframe: false} );
+	var RingCore = new __WEBPACK_IMPORTED_MODULE_0_three__["Mesh"]( geometry, material );
+	scene.add( RingCore )
+
+	var geometry = new __WEBPACK_IMPORTED_MODULE_0_three__["TorusGeometry"]( 202,20, 10, 100);
+	var material = new __WEBPACK_IMPORTED_MODULE_0_three__["MeshLambertMaterial"]( { color:  "rgba(94, 236, 255,0.4)" , morphTargets:true, wireframe: true} );
+	var RingWire = new __WEBPACK_IMPORTED_MODULE_0_three__["Mesh"]( geometry, material );
+	scene.add( RingWire );
+	RingCore.position.z = 150;
+	RingWire.position.z = 150;
+	RingCore.position.y = 150;
+	RingWire.position.y = 150;
+	RingCore.position.x = -150;
+	RingWire.position.x = -150;
+
+	/*=================================================
+		LIGHTS
+	=================================================*/
+
+
+
+			scene.add( new __WEBPACK_IMPORTED_MODULE_0_three__["AmbientLight"]( 0x222222 ) );
+	                      
+	                      
+	                      
+	    	light = new __WEBPACK_IMPORTED_MODULE_0_three__["DirectionalLight"]( 0x222222 );
+					light.position.set( 1, 1, 1 );
+					scene.add( light );
+					light = new __WEBPACK_IMPORTED_MODULE_0_three__["DirectionalLight"]( 'tomato' );
+					light.position.set( -200, -200, -200 );
+					scene.add( light );
+					light = new __WEBPACK_IMPORTED_MODULE_0_three__["AmbientLight"]( 0x222222 );
+					scene.add( light );
+					
+		    var hemisphereLight = new __WEBPACK_IMPORTED_MODULE_0_three__["HemisphereLight"](0xaaaaaa,0x000000, .9);
+		    scene.add(hemisphereLight);
+			
 		
-var  objects = [];			
-renderer.setSize( window.innerWidth-15, window.innerHeight-15 );
-document.getElementById('Orbit').appendChild( renderer.domElement );
-/*		
-controls = new OrbitControls( camera, renderer.domElement );
-controls.enableDamping = true;
-controls.dampingFactor = 0.25;
-controls.enableZoom = true;
-*/
+	/*=================================================
+		RANDOM ELEMENTS
+	=================================================*/			
+					
 
-camera.position.z = 520;
-camera.position.x = -0;
-
-var raycaster = new __WEBPACK_IMPORTED_MODULE_0_three__["Raycaster"]();
-var mouse = new __WEBPACK_IMPORTED_MODULE_0_three__["Vector2"]();
-
-function onMouseMove( event ) {
-
-	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;		
-}
-
-window.addEventListener( 'mousemove', onMouseMove, false );
+	var geometry = new __WEBPACK_IMPORTED_MODULE_0_three__["SphereGeometry"]( 1, 32, 32 );
+	var material = new __WEBPACK_IMPORTED_MODULE_0_three__["MeshLambertMaterial"]( { color:  0xFDFDFDF, morphTargets:true, wireframe: false} );
 
 
-	
+	    	for ( var i = 0; i < 200; i ++ ) {
+		  		   
+						var cube = new __WEBPACK_IMPORTED_MODULE_0_three__["Mesh"]( geometry, material );
+						cube.position.x = ( Math.random() - 0.5 ) * 1200;
+						cube.position.y = ( Math.random() - 0.5 ) * 1200;
+						cube.position.z = ( Math.random() - 0.5 ) * 1200;
+						cube.updateMatrix();
+						cube.matrixAutoUpdate = false;
+						scene.add( cube );
+			}
 
-/*=================================================
-	MAIN ELEMENTS
-=================================================*/			
-				
-var geometry = new __WEBPACK_IMPORTED_MODULE_0_three__["SphereGeometry"]( 105, 302, 302 );
-//var geometry = new THREE.TorusGeometry( 122, 90 , 20, 300, 100);
-var material = new __WEBPACK_IMPORTED_MODULE_0_three__["MeshLambertMaterial"]( { color:  "rgba(35, 138, 190,0.1)", morphTargets:true, wireframe: false} );
-var CenterCore = new __WEBPACK_IMPORTED_MODULE_0_three__["Mesh"]( geometry, material );
-CenterCore.name = "Bli";
+			this.light = new __WEBPACK_IMPORTED_MODULE_0_three__["PointLight"]();
+	        this.light.position.set(0, 0,0);
+	        this.scene.add(this.light);
 
-CenterCore.position.z = 16;
+	/*=================================================
+		RENDER 
+	=================================================*/
 
-scene.add( CenterCore );
-objects.push( CenterCore );
-
-var geometry = new __WEBPACK_IMPORTED_MODULE_0_three__["TorusGeometry"]( 122, 102 , 15, 15, 20);
-var material = new __WEBPACK_IMPORTED_MODULE_0_three__["MeshLambertMaterial"]( { color:  "rgba(35, 138, 190,0.1)", morphTargets:true, wireframe: false} );
-var CenterWire = new __WEBPACK_IMPORTED_MODULE_0_three__["Mesh"]( geometry, material );
-CenterWire.position.z = 16;
-
-//scene.add( CenterWire )
-
-
-projector = new __WEBPACK_IMPORTED_MODULE_0_three__["Projector"]();
-
-
-
-function onDocumentMouseDown( event ) {
-
-    event.preventDefault();
-    
-    var vector = new __WEBPACK_IMPORTED_MODULE_0_three__["Vector3"]( 
-        ( event.clientX / window.innerWidth ) * 2 - 1, 
-        - ( event.clientY / window.innerHeight ) * 2 + 1, 
-        0.5 );
-    
-    projector.unprojectVector( vector, camera );
-    
-    var ray = new __WEBPACK_IMPORTED_MODULE_0_three__["Raycaster"]( camera.position, vector.sub( camera.position ).normalize() );
-
-    var intersects = ray.intersectObjects( objects );    
-    if ( intersects.length > 0 ) {
-                
-		intersects[0].object.name;
-        alert(intersects[0].object.name);
-    }
-                    
-}
-
-document.addEventListener( 'mousedown', onDocumentMouseDown, false);
-
-
-
-
-var geometry = new __WEBPACK_IMPORTED_MODULE_0_three__["SphereGeometry"]( 105, 302, 302 );
-//var geometry = new THREE.TorusGeometry( 122, 90 , 20, 300, 100);
-var material = new __WEBPACK_IMPORTED_MODULE_0_three__["MeshLambertMaterial"]( { color:  0xF02622, morphTargets:true, wireframe: false} );
-var redPlanet = new __WEBPACK_IMPORTED_MODULE_0_three__["Mesh"]( geometry, material );
-redPlanet.position.x = 506;
-redPlanet.name = "Sun";
-
-scene.add( redPlanet );
-
-objects.push( redPlanet );
-
-
-
-
-var geometry = new __WEBPACK_IMPORTED_MODULE_0_three__["TorusGeometry"]( 302, 15, 10, 150);
-var material = new __WEBPACK_IMPORTED_MODULE_0_three__["MeshLambertMaterial"]( { color:  "rgba(4, 4, 4,1)" , morphTargets:true, wireframe: false} );
-var RingCore = new __WEBPACK_IMPORTED_MODULE_0_three__["Mesh"]( geometry, material );
-scene.add( RingCore )
-
-var geometry = new __WEBPACK_IMPORTED_MODULE_0_three__["TorusGeometry"]( 302,20, 10, 100);
-var material = new __WEBPACK_IMPORTED_MODULE_0_three__["MeshLambertMaterial"]( { color:  "rgba(94, 236, 255,0.4)" , morphTargets:true, wireframe: true} );
-var RingWire = new __WEBPACK_IMPORTED_MODULE_0_three__["Mesh"]( geometry, material );
-scene.add( RingWire );
-
-
-var sphere = new __WEBPACK_IMPORTED_MODULE_0_three__["Mesh"](
-  new __WEBPACK_IMPORTED_MODULE_0_three__["SphereGeometry"]( 15, 32, 302 ),
-  new __WEBPACK_IMPORTED_MODULE_0_three__["MeshBasicMaterial"]({
-    map: __WEBPACK_IMPORTED_MODULE_0_three__["ImageUtils"].loadTexture('http://kniessner.com/comPlex/wp-content/themes/complex/_/img/logo.png')
-
-  })
-);
-
-sphere.position.z = 40;
-
-sphere.position.x = 400;
-
-
-scene.add(sphere);
-
-
-
-
-/*=================================================
-	LIGHTS
-=================================================*/
-
-
-
-		 scene.add( new __WEBPACK_IMPORTED_MODULE_0_three__["AmbientLight"]( 0x222222 ) );
-                      
-                      
-                      
-    	light = new __WEBPACK_IMPORTED_MODULE_0_three__["DirectionalLight"]( 0xffffff );
-				light.position.set( 1, 1, 1 );
-				scene.add( light );
-				light = new __WEBPACK_IMPORTED_MODULE_0_three__["DirectionalLight"]( 0x002288 );
-				light.position.set( -200, -200, -200 );
-				scene.add( light );
-				light = new __WEBPACK_IMPORTED_MODULE_0_three__["AmbientLight"]( 0x222222 );
-				scene.add( light );
-				
-	    var hemisphereLight = new __WEBPACK_IMPORTED_MODULE_0_three__["HemisphereLight"](0xaaaaaa,0x000000, .9);
-	    scene.add(hemisphereLight);
-
-		this.light = new __WEBPACK_IMPORTED_MODULE_0_three__["PointLight"]();
-        this.light.position.set(-256, 256, -256);
-        this.scene.add(this.light);
-        
-        
-
-
-/*=================================================
-	Floor Example 
-=================================================*/
-        
-        	/*
-        		geometry = new THREE.PlaneGeometry( 2000, 2000, 100, 100 );
-				geometry.rotateX( - Math.PI / 2 );
-				for ( var i = 0, l = geometry.vertices.length; i < l; i ++ ) {
-					var vertex = geometry.vertices[ i ];
-					vertex.x += Math.random() * 20 - 10;
-					vertex.y += Math.random() * 2;
-					vertex.z += Math.random() * 20 - 10;
-				}
-				
-				
-				for ( var i = 0, l = geometry.faces.length; i < l; i ++ ) {
-					var face = geometry.faces[ i ];
-					face.vertexColors[ 0 ] = new THREE.Color().setHSL( Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
-					face.vertexColors[ 1 ] = new THREE.Color().setHSL( Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
-					face.vertexColors[ 2 ] = new THREE.Color().setHSL( Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
-				}
-				material = new THREE.MeshBasicMaterial( { vertexColors: THREE.VertexColors } );
-				mesh = new THREE.Mesh( geometry, material );
-				scene.add( mesh );*/
-/*=================================================
-	RENDER 
-=================================================*/
-document.addEventListener('keydown',onDocumentKeyDown,false);
-function onDocumentKeyDown(event){
-var delta = 20;
-event = event || window.event;
-var keycode = event.keyCode;
-switch(keycode){
-case 37 : //left arrow 向左箭头
-camera.position.x = camera.position.x - delta;
-camera.updateProjectionMatrix();
-break;
-case 38 : // up arrow 向上箭头
-camera.position.z = camera.position.z - delta;
-camera.updateProjectionMatrix();
-break;
-case 39 : // right arrow 向右箭头
-camera.position.x = camera.position.x + delta;
-camera.updateProjectionMatrix();
-break;
-case 40 : //down arrow向下箭头
-camera.position.z = camera.position.z + delta;
-camera.updateProjectionMatrix();
-break;
-}
-//document.addEventListener('keyup',onDocumentKeyUp,false);
-}
-function onDocumentKeyUp(event){
-document.removeEventListener('keydown',onDocumentKeyDown,false);
-}
-
-
-
-
-
-
-var render = function () { 
-	camera.rotation.z -= 0.001;
-	requestAnimationFrame(render); 
-	renderer.render(scene, camera); 
-}; 
-      
+	var render = function () { 
+		
+			
+			
+			RingWire.rotation.x += 0.006;
+	 		RingCore.rotation.x += 0.006; 		
+	 		RingWire.rotation.y += 0.003;
+	 		RingCore.rotation.y += 0.003;
+	 		RingWire.rotation.z += 0.0006;
+	 		RingCore.rotation.z += 0.0006;
+	 		camera.rotation.z -= 0.0006;
+	 		
+	        requestAnimationFrame(render); 
+	        renderer.render(scene, camera); 
+	        
+	        
+	      
+	}; 
+	      
 render();  
-
 });
 
 		
--- src/js/orbit.js
+++ src/js/orbit.js
@@ -8,233 +8,109 @@ var OrbitControls = require('three-orbit-controls')(THREE);
 jQuery(document).ready(function($) {
 
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 60, (window.innerWidth-15) / (window.innerHeight-15), 1, 1000 );
-var renderer = new THREE.WebGLRenderer();
-var raycaster;
+	var scene = new THREE.Scene();
+	var camera = new THREE.PerspectiveCamera( 60, (window.innerWidth-15) / (window.innerHeight-15), 1, 1000 );
+	var renderer = new THREE.WebGLRenderer();
+	var raycaster;
+			
+	renderer.setSize( window.innerWidth-15, (window.innerHeight) );
+	document.getElementById('Orbit').appendChild( renderer.domElement );
+	camera.position.z = 520;
+	camera.position.x = -0;
+
+
+	/*=================================================
+		MAIN ELEMENTS
+	=================================================*/	
+
+
+	var geometry = new THREE.TorusGeometry( 202, 15, 10, 150);
+	var material = new THREE.MeshLambertMaterial( { color:  "rgba(4, 4, 4,1)" , morphTargets:true, wireframe: false} );
+	var RingCore = new THREE.Mesh( geometry, material );
+	scene.add( RingCore )
+
+	var geometry = new THREE.TorusGeometry( 202,20, 10, 100);
+	var material = new THREE.MeshLambertMaterial( { color:  "rgba(94, 236, 255,0.4)" , morphTargets:true, wireframe: true} );
+	var RingWire = new THREE.Mesh( geometry, material );
+	scene.add( RingWire );
+	RingCore.position.z = 150;
+	RingWire.position.z = 150;
+	RingCore.position.y = 150;
+	RingWire.position.y = 150;
+	RingCore.position.x = -150;
+	RingWire.position.x = -150;
+
+	/*=================================================
+		LIGHTS
+	=================================================*/
+
+
+
+			scene.add( new THREE.AmbientLight( 0x222222 ) );
+	                      
+	                      
+	                      
+	    	light = new THREE.DirectionalLight( 0x222222 );
+					light.position.set( 1, 1, 1 );
+					scene.add( light );
+					light = new THREE.DirectionalLight( 'tomato' );
+					light.position.set( -200, -200, -200 );
+					scene.add( light );
+					light = new THREE.AmbientLight( 0x222222 );
+					scene.add( light );
+					
+		    var hemisphereLight = new THREE.HemisphereLight(0xaaaaaa,0x000000, .9);
+		    scene.add(hemisphereLight);
+			
 		
-var  objects = [];			
-renderer.setSize( window.innerWidth-15, window.innerHeight-15 );
-document.getElementById('Orbit').appendChild( renderer.domElement );
-/*		
-controls = new OrbitControls( camera, renderer.domElement );
-controls.enableDamping = true;
-controls.dampingFactor = 0.25;
-controls.enableZoom = true;
-*/
+	/*=================================================
+		RANDOM ELEMENTS
+	=================================================*/			
+					
 
-camera.position.z = 520;
-camera.position.x = -0;
-
-var raycaster = new THREE.Raycaster();
-var mouse = new THREE.Vector2();
-
-function onMouseMove( event ) {
-
-	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;		
-}
-
-window.addEventListener( 'mousemove', onMouseMove, false );
+	var geometry = new THREE.SphereGeometry( 1, 32, 32 );
+	var material = new THREE.MeshLambertMaterial( { color:  0xFDFDFDF, morphTargets:true, wireframe: false} );
 
 
-	
+	    	for ( var i = 0; i < 200; i ++ ) {
+		  		   
+						var cube = new THREE.Mesh( geometry, material );
+						cube.position.x = ( Math.random() - 0.5 ) * 1200;
+						cube.position.y = ( Math.random() - 0.5 ) * 1200;
+						cube.position.z = ( Math.random() - 0.5 ) * 1200;
+						cube.updateMatrix();
+						cube.matrixAutoUpdate = false;
+						scene.add( cube );
+			}
 
-/*=================================================
-	MAIN ELEMENTS
-=================================================*/			
-				
-var geometry = new THREE.SphereGeometry( 105, 302, 302 );
-//var geometry = new THREE.TorusGeometry( 122, 90 , 20, 300, 100);
-var material = new THREE.MeshLambertMaterial( { color:  "rgba(35, 138, 190,0.1)", morphTargets:true, wireframe: false} );
-var CenterCore = new THREE.Mesh( geometry, material );
-CenterCore.name = "Bli";
+			this.light = new THREE.PointLight();
+	        this.light.position.set(0, 0,0);
+	        this.scene.add(this.light);
 
-CenterCore.position.z = 16;
+	/*=================================================
+		RENDER 
+	=================================================*/
 
-scene.add( CenterCore );
-objects.push( CenterCore );
-
-var geometry = new THREE.TorusGeometry( 122, 102 , 15, 15, 20);
-var material = new THREE.MeshLambertMaterial( { color:  "rgba(35, 138, 190,0.1)", morphTargets:true, wireframe: false} );
-var CenterWire = new THREE.Mesh( geometry, material );
-CenterWire.position.z = 16;
-
-//scene.add( CenterWire )
-
-
-projector = new THREE.Projector();
-
-
-
-function onDocumentMouseDown( event ) {
-
-    event.preventDefault();
-    
-    var vector = new THREE.Vector3( 
-        ( event.clientX / window.innerWidth ) * 2 - 1, 
-        - ( event.clientY / window.innerHeight ) * 2 + 1, 
-        0.5 );
-    
-    projector.unprojectVector( vector, camera );
-    
-    var ray = new THREE.Raycaster( camera.position, vector.sub( camera.position ).normalize() );
-
-    var intersects = ray.intersectObjects( objects );    
-    if ( intersects.length > 0 ) {
-                
-		intersects[0].object.name;
-        alert(intersects[0].object.name);
-    }
-                    
-}
-
-document.addEventListener( 'mousedown', onDocumentMouseDown, false);
-
-
-
-
-var geometry = new THREE.SphereGeometry( 105, 302, 302 );
-//var geometry = new THREE.TorusGeometry( 122, 90 , 20, 300, 100);
-var material = new THREE.MeshLambertMaterial( { color:  0xF02622, morphTargets:true, wireframe: false} );
-var redPlanet = new THREE.Mesh( geometry, material );
-redPlanet.position.x = 506;
-redPlanet.name = "Sun";
-
-scene.add( redPlanet );
-
-objects.push( redPlanet );
-
-
-
-
-var geometry = new THREE.TorusGeometry( 302, 15, 10, 150);
-var material = new THREE.MeshLambertMaterial( { color:  "rgba(4, 4, 4,1)" , morphTargets:true, wireframe: false} );
-var RingCore = new THREE.Mesh( geometry, material );
-scene.add( RingCore )
-
-var geometry = new THREE.TorusGeometry( 302,20, 10, 100);
-var material = new THREE.MeshLambertMaterial( { color:  "rgba(94, 236, 255,0.4)" , morphTargets:true, wireframe: true} );
-var RingWire = new THREE.Mesh( geometry, material );
-scene.add( RingWire );
-
-
-var sphere = new THREE.Mesh(
-  new THREE.SphereGeometry( 15, 32, 302 ),
-  new THREE.MeshBasicMaterial({
-    map: THREE.ImageUtils.loadTexture('http://kniessner.com/comPlex/wp-content/themes/complex/_/img/logo.png')
-
-  })
-);
-
-sphere.position.z = 40;
-
-sphere.position.x = 400;
-
-
-scene.add(sphere);
-
-
-
-
-/*=================================================
-	LIGHTS
-=================================================*/
-
-
-
-		 scene.add( new THREE.AmbientLight( 0x222222 ) );
-                      
-                      
-                      
-    	light = new THREE.DirectionalLight( 0xffffff );
-				light.position.set( 1, 1, 1 );
-				scene.add( light );
-				light = new THREE.DirectionalLight( 0x002288 );
-				light.position.set( -200, -200, -200 );
-				scene.add( light );
-				light = new THREE.AmbientLight( 0x222222 );
-				scene.add( light );
-				
-	    var hemisphereLight = new THREE.HemisphereLight(0xaaaaaa,0x000000, .9);
-	    scene.add(hemisphereLight);
-
-		this.light = new THREE.PointLight();
-        this.light.position.set(-256, 256, -256);
-        this.scene.add(this.light);
-        
-        
-
-
-/*=================================================
-	Floor Example 
-=================================================*/
-        
-        	/*
-        		geometry = new THREE.PlaneGeometry( 2000, 2000, 100, 100 );
-				geometry.rotateX( - Math.PI / 2 );
-				for ( var i = 0, l = geometry.vertices.length; i < l; i ++ ) {
-					var vertex = geometry.vertices[ i ];
-					vertex.x += Math.random() * 20 - 10;
-					vertex.y += Math.random() * 2;
-					vertex.z += Math.random() * 20 - 10;
-				}
-				
-				
-				for ( var i = 0, l = geometry.faces.length; i < l; i ++ ) {
-					var face = geometry.faces[ i ];
-					face.vertexColors[ 0 ] = new THREE.Color().setHSL( Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
-					face.vertexColors[ 1 ] = new THREE.Color().setHSL( Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
-					face.vertexColors[ 2 ] = new THREE.Color().setHSL( Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75 );
-				}
-				material = new THREE.MeshBasicMaterial( { vertexColors: THREE.VertexColors } );
-				mesh = new THREE.Mesh( geometry, material );
-				scene.add( mesh );*/
-/*=================================================
-	RENDER 
-=================================================*/
-document.addEventListener('keydown',onDocumentKeyDown,false);
-function onDocumentKeyDown(event){
-var delta = 20;
-event = event || window.event;
-var keycode = event.keyCode;
-switch(keycode){
-case 37 : //left arrow 向左箭头
-camera.position.x = camera.position.x - delta;
-camera.updateProjectionMatrix();
-break;
-case 38 : // up arrow 向上箭头
-camera.position.z = camera.position.z - delta;
-camera.updateProjectionMatrix();
-break;
-case 39 : // right arrow 向右箭头
-camera.position.x = camera.position.x + delta;
-camera.updateProjectionMatrix();
-break;
-case 40 : //down arrow向下箭头
-camera.position.z = camera.position.z + delta;
-camera.updateProjectionMatrix();
-break;
-}
-//document.addEventListener('keyup',onDocumentKeyUp,false);
-}
-function onDocumentKeyUp(event){
-document.removeEventListener('keydown',onDocumentKeyDown,false);
-}
-
-
-
-
-
-
-var render = function () { 
-	camera.rotation.z -= 0.001;
-	requestAnimationFrame(render); 
-	renderer.render(scene, camera); 
-}; 
-      
+	var render = function () { 
+		
+			
+			
+			RingWire.rotation.x += 0.006;
+	 		RingCore.rotation.x += 0.006; 		
+	 		RingWire.rotation.y += 0.003;
+	 		RingCore.rotation.y += 0.003;
+	 		RingWire.rotation.z += 0.0006;
+	 		RingCore.rotation.z += 0.0006;
+	 		camera.rotation.z -= 0.0006;
+	 		
+	        requestAnimationFrame(render); 
+	        renderer.render(scene, camera); 
+	        
+	        
+	      
+	}; 
+	      
 render();  
-
 });
 
 		
