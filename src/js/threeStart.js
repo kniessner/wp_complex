8f4ae1f28e87ac022a4f42c3efeb61fb4d5c17db
Sascha-Darius Knießner <Dariuskniessner@littlewebgiants.com>
2017-10-28 06:56:12 +0200

    automatic commit

--- src/js/threeStart.js
+++ src/js/threeStart.js
@@ -33,10 +33,10 @@ jQuery(document).ready(function($) {
 /*=================================================
 		MAIN ELEMENTS
 =================================================*/	
-// Set up the sphere vars
-const RADIUS = 50;
-const SEGMENTS = 16;
-const RINGS = 16;
+				// Set up the sphere vars
+				const RADIUS = 50;
+				const SEGMENTS = 16;
+				const RINGS = 16;
 
 
 				scene.fog = new THREE.FogExp2( 0x000000, 0.0007 );
@@ -81,67 +81,6 @@ const RINGS = 16;
 
 
 
-// Create a new mesh with
-// sphere geometry - we will cover
-// the sphereMaterial next!
-var material = new THREE.MeshLambertMaterial( { color:  0xFDFDFDF, morphTargets:true ,wireframe: true,combine:THREE.MultiplyBlending} );
-
-const sphere = new THREE.Mesh(
-
-  new THREE.SphereGeometry(
-    RADIUS,
-    SEGMENTS,
-    RINGS),
-  material);
-
-// Move the Sphere back in Z so we
-// can see it.
-sphere.position.z = -300;
-
-// Finally, add the sphere to the scene.
-scene.add(sphere);
-
-
-var geometry = new THREE.TorusGeometry( 11, 0.5, 16, 100 );
-var mergeGeometry = new THREE.Geometry();
-var matrix = new THREE.Matrix4();
-
-for( i = 1; i <= 50; i++ ) {
-
-    matrix.makeTranslation( 0, 3.4 * i, 0 );
-    mergeGeometry.merge( geometry, matrix );
-
-}
-
-var mesh = new THREE.Mesh( mergeGeometry, material )
-
-var geometry = new THREE.SphereGeometry( 152,15, 15 ); // radius - segments -rings
-
-				
-var geometryCore = new THREE.SphereGeometry( 115,15, 15 ); // radius - segments -rings
-var materialCore = new THREE.MeshPhongMaterial( { color:  0xFDFDFDF, shading:THREE.FlatShading, morphTargets:true ,wireframe: false,combine:THREE.MultiplyBlending} );
-	
-var ball = new THREE.Mesh( geometry, material );
-var ballcore = new THREE.Mesh( geometryCore, materialCore );
-
-
-// List of all the materials used in the meshes you want to combine
-var materials = [materialCore, material]; 
-
-// List of the meshes you want to combine, for each one you have to store the index of the material within the materials array 
-var meshes = [{mesh: ball, materialIndex:0}, {mesh: ballcore, materialIndex:1}, {mesh: sphere, materialIndex:0}];
-
-// Geometry of the combined mesh
-var totalGeometry = new THREE.Geometry();
-for(var i = 0; i < meshes.length; i++)
-{
-    meshes[i].mesh.updateMatrix();
-    geometryCore.merge(meshes[i].mesh.geometry, meshes[i].mesh.matrix, meshes[i].materialIndex);
-}
-
-// Create the combined mesh
-var combinedMesh = new THREE.Mesh(geometryCore, new THREE.MeshFaceMaterial(materials));
-scene.add(combinedMesh);
 /*=================================================
 		LIGHTS
 =================================================*/
