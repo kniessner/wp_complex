6bae0601938ac20dfc03331a014d30d4d46836c5
Sascha-Darius Knießner <Dariuskniessner@littlewebgiants.com>
2017-10-28 02:04:49 +0200

    automatic commit

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,30 +1,9 @@
 
 $(document).ready(function(){
   $('.slider').slick({
-  		  centerMode: true,
-		  centerPadding: '60px',
-		  slidesToShow: 3,
+		  slidesToShow: 1,
 		  autoplay:true,
-		  arrows:false,
-		  responsive: [
-		    {
-		      breakpoint: 768,
-		      settings: {
-		        arrows: false,
-		        centerMode: true,
-		        centerPadding: '40px',
-		        slidesToShow: 3
-		      }
-		    },
-		    {
-		      breakpoint: 480,
-		      settings: {
-		        arrows: false,
-		        centerMode: true,
-		        centerPadding: '40px',
-		        slidesToShow: 3
-		      }
-		    }
-		  ]
+		  arrows:false
+		 
   });
 });
