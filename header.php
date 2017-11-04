7e6de5d06b76e3f242897e8b198fd8283e947c4c
Sascha-Darius Knießner <Dariuskniessner@littlewebgiants.com>
2017-11-04 21:27:02 +0100

    automatic commit

--- header.php
+++ header.php
@@ -45,7 +45,7 @@
 
 </header>
   
-  <nav class="navbar navbar-toggleable-md sticky-top bg-faded" id="main_menu">
+  <nav class="navbar navbar-expand-md sticky-top bg-faded" id="main_menu">
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
