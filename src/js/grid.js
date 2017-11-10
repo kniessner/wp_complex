494e81141cff7589af697c20d2e91e5c274605d2
Sascha-Darius Knießner <Dariuskniessner@littlewebgiants.com>
2017-11-10 02:49:11 +0100

    automatic commit

--- src/js/grid.js
+++ src/js/grid.js
@@ -7,9 +7,10 @@ console.log('Masonry',Masonry);
 const grid = document.querySelector('.masonry-grid')
 const msnry = new Masonry(grid, {
   itemSelector: '.grid-item',
-  columnWidth: 280,
-  gutter: 10,
+  columnWidth: 33,
+  //gutter: 10,
   transitionDuration: 0,
+  percentPosition: true,
   initLayout: false
 })
 
