99ba61a833ae04bb7abd1f7615201fab814bd35c
Sascha-Darius Knießner <Dariuskniessner@littlewebgiants.com>
2017-11-11 05:34:20 +0100

    automatic commit

--- src/components/image_grid.jsx
+++ src/components/image_grid.jsx
@@ -2,17 +2,34 @@ import React, {Component} from 'react';
 import {render} from 'react-dom';
 import Masonry from 'react-masonry-component'; //https://github.com/eiriklv/react-masonry-component
 
+
 const masonryOptions = {
-    transitionDuration: 0
+    transitionDuration: 0,
+    itemSelector: '.grid-item',
+    columnWidth: 25,
+    gutter: 0,
+    percentPosition: true,
 };
    
+
 class Image_Grid extends React.Component {
+  
+    handleLayoutComplete() { 
+        console.log('layout complete');
+    }
+
+   componentDidMount() {
+       this.masonry.on('layoutComplete', this.handleLayoutComplete);
+   }
+
+   componentWillUnmount() {
+       this.masonry.off('layoutComplete', this.handleLayoutComplete);
+   }
 
   render () {
-       
         var childElements = this.props.photos.map(function(image){
            return (
-                <li className="image-element-class">
+                <li className="grid-item" key={image.id}>
                     <img src={image.sizes.medium} />
                 </li>
             );
@@ -20,6 +37,7 @@ class Image_Grid extends React.Component {
 
         return (
             <Masonry
+                ref={function(c) {this.masonry = this.masonry || c.masonry;}.bind(this)}
                 className={'grids'} // default ''
                 options={masonryOptions} // default {}
                 disableImagesLoaded={false} // default false
