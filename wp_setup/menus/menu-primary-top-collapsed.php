43227524b136d3c49ef68f8620e1593a22e3a71a
Sascha-Darius Knießner <Dariuskniessner@littlewebgiants.com>
2017-11-18 12:10:47 +0100

    automatic commit

--- wp_setup/menus/menu-primary-top-collapsed.php
+++ wp_setup/menus/menu-primary-top-collapsed.php
@@ -9,32 +9,22 @@
  ?>
 
 <div class="menu-primary-top-collapsed">
-  
   <nav class="collapse" id="main_menu">
       <?php if ( has_nav_menu( 'primary' ) ) : ?>
 
 		  <?php 
-      //navbar-nav
-
-      wp_nav_menu( array( 'theme_location' => 'primary', 'container' => false, 'menu_class' => ' mr-auto ', 'menu_id' => 'menu-primary-items', 'fallback_cb' => '' ) ); ?>
+      wp_nav_menu( array( 'theme_location' => 'primary', 'container' => false, 'menu_class' => ' mr-auto ', 'menu_id' => 'menu-primary-items', 'fallback_cb' => '' ) ); 
+      ?>
 
 		  <?php 
 		//mt-2 mt-md-0
 		endif; ?>
-
   </nav>
 
-<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main_menu" aria-controls="main_menu" aria-expanded="false" aria-label="Toggle navigation">
-     <i class="fa fa-bars" aria-hidden="true"></i>
-</button>
-
-  <div class="action_panel">
-    <i class="fa fa-linkedin" aria-hidden="true"></i>
-    <i class="fa fa-github" aria-hidden="true"></i>
-    <i class="fa fa-envelope" aria-hidden="true"></i>
-    <i class="fa fa-flickr" aria-hidden="true"></i>
-
-  </div>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main_menu" aria-controls="main_menu" aria-expanded="false" aria-label="Toggle navigation">
+       <i class="fa fa-bars" aria-hidden="true"></i>
+  </button>
+  
 </div>
 
 
