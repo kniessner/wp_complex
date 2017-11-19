4efc04c89cde686b069eba5f3810612d3e920fb0
Sascha-Darius Knießner <Dariuskniessner@littlewebgiants.com>
2017-11-19 14:15:06 +0100

    automatic commit

--- functions.php
+++ functions.php
@@ -114,6 +114,18 @@ add_action( 'send_headers', function() {
 		header( 'Access-Control-Allow-Methods: HEAD' );
 	}
 } );
+add_action( 'rest_api_init', function() {
+    
+	remove_filter( 'rest_pre_serve_request', 'rest_send_cors_headers' );
+	add_filter( 'rest_pre_serve_request', function( $value ) {
+		header( 'Access-Control-Allow-Origin: *' );
+		header( 'Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE' );
+		header( 'Access-Control-Allow-Credentials: true' );
+
+		return $value;
+		
+	});
+}, 15 );
 /*
 function my_customize_rest_cors() {
   remove_filter( 'rest_pre_serve_request', 'rest_send_cors_headers' );
