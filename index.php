f4b577e930b9fec710d09168d2e2edf3b20a49ea
Sascha-Darius Knießner <dariuskniessner@littlewebgiants.com>
2017-12-08 22:46:20 +0100

    slim

--- index.php
+++ index.php
@@ -17,19 +17,6 @@ get_header(); ?>
 		<main id="main" class="site-main">
 		<!--<div class="start_image" ><img class="screen_fit" src="<?php bloginfo('template_url');?>/src/img/logo_form.png" /> </div>-->
 
-		Test
-		<div class="skill_slider">
-				<div class="word_base"><h4>Complex</h4></div>
-				<div class="slider word_slider">
-					  <div class="slide"><h4> Design</h4></div>
-					  <div class="slide"><h4> Photography</h4></div>
-					  <div class="slide"><h4> Art</h4></div>
-					  <div class="slide"><h4> Care</h4></div>
-					  <div class="slide"><h4> Concepts</h4></div>
-					  <div class="slide"><h4> Experiences</h4></div>
-				</div>
-		</div>
-
 		 <?php if ( have_posts() ) : ?>
              <?php while ( have_posts() ) : the_post(); ?>
 					<div class="container">
